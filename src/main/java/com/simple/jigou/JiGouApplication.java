c3e34c3af93c0890591565e858fc63a367095563
Shine4Smile <1853967891@qq.com>
2026-09-08 20:27:23 +0800

    chore: 初始化springboot项目结构

---
+++ src/main/java/com/simple/jigou/JiGouApplication.java
@@ -0,0 +1,13 @@
+package com.simple.jigou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiGouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiGouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simple/jigou/JiGouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.jigou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiGouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
