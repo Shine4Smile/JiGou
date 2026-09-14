e96a7b33ffa46c2faf2adab6404228d40666d534
Shine4Smile <1853967891@qq.com>
2026-09-14 15:45:36 +0800

    feat(backend): 门面模式处理模型生成的代码，写入文件

---
+++ src/test/java/com/simple/jigou/core/AiCodeGeneratorFacadeTest.java
@@ -0,0 +1,22 @@
+package com.simple.jigou.core;
+
+import com.simple.jigou.model.enums.CodeGenTypeEnum;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+@SpringBootTest
+class AiCodeGeneratorFacadeTest {
+
+    @Resource
+    private AiCodeGeneratorFacade aiCodeGeneratorFacade;
+
+    @Test
+    void generateAndSaveCode() {
+        File file = aiCodeGeneratorFacade.generateAndSaveCode("每日计划打卡网站", CodeGenTypeEnum.MULTI_FILE);
+        Assertions.assertNotNull(file);
+    }
+}
